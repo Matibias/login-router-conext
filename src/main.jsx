32fb1993dfaddce3e7ecb68dca597c1e3d9ad0ad
matibias <mparrab103@gmail.com>
2023-03-31 22:05:20 -0300

    style: add index css

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterApp } from './RouterApp'
 import { BrowserRouter } from "react-router-dom";
 
-
+import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <React.StrictMode>
